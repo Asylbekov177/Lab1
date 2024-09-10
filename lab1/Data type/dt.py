cec5fd82a23dfe4a8541d8bcaa15b10b82ed7bcb
Mukhamedali Asylbek <65166989+Asylbekov177@users.noreply.github.com>
2024-09-10 12:28:29 +0500

    Add files via upload

---
+++ lab1/Data type/dt.py
@@ -0,0 +1,121 @@
+x="Hello World" #str type
+
+print(x)
+ 
+print(type(x))
+ 
+print("________________") 
+x=20 #int type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+x=20.5 #float type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+x=1j #complex type
+
+print(x)
+
+print(type(x))
+    
+print("________________")
+x=["apple", "banana", "cherry"] #list type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x=("apple", "banana", "cherry") #tuple type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x=range(6) #range type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x={"name" : "John", "age" : 36} #dict type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x={"apple", "banana", "cherry"} #set type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x=frozenset({"apple", "banana", "cherry"}) #frozenset type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x=True #bool type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x=b"Hello" #bytes type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x = bytearray(5) #bytearray
+
+print(x)
+
+print(type(x)) 
+
+print("________________")
+
+x = bytearray(5) #bytearray
+
+print(x)
+
+print(type(x)) 
+
+print("________________")
+
+x = memoryview(bytes(5)) #memoryview type
+
+print(x)
+
+print(type(x))
+
+print("________________")
+
+x = None #Nonetype
+
+print(x)
+
+print(type(x))
